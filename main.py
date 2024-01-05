903bb0fa417f9ced1ad787f09687598216b1d5db
Amogh Pathak <amogh9792@gmail.com>
2024-01-05 16:23:20 +0530

    everthing done

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from source.entity.config_entity import TrainingPipelineConfig
+if __name__ == '__main__':
+    Train_pipeline_config_obj = TrainingPipelineConfig()
+
+    print(Train_pipeline_config_obj.__dict__)
+
+
+
+
---
+++ source/constant/constant_train.py
@@ -0,0 +1,18 @@
+# Common constants
+TARGET_COLUMN = "Churn"
+TRAIN_PIPELINE_NAME = "train_pipeline"
+ARTIFACT_DIR = "artifact"
+FILE_NAME = "training_data"
+
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+
+MONGODB_URL_KEY = "mongodb+srv://amogh9792:bluehorse1998@amogh.zciekxq.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = "db-customer-chunk"
+
+# Data Ingestion Variables
+DI_COLLECTION_NAME = "telecom-collection-churn"
+DI_DIR_NAME = "data_ingestion"
+DI_FEATURE_STORE_DIR = "feature_store"
+DI_INGESTED_DIR = "ingested"
+DI_TRAIN_TEST_SPLIT_RATIO = 0.2
